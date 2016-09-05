d2b4ff384d36f1b5c9f8979980541b724fb50a4f
Irma Karasoftić <irmaka@mistral.ba>
2016-09-05 16:33:34 +0200

    added crud functions to repository

--- TaskDb/Repository.cs
+++ TaskDb/Repository.cs
@@ -7,20 +7,57 @@ using System.Threading.Tasks;
 
 namespace TaskDb
 {
-    public class Repository<E> where E:class
+    public class Repository<Entity> where Entity:class
     {
         public Context context;
-        public DbSet<E> dbSet;
+        public DbSet<Entity> dbSet;
 
         public Repository(Context _context)
         {
             context = _context;
-            dbSet = _context.Set<E>();
+            dbSet = _context.Set<Entity>();
         }
 
         public Context HomeContext()
         {
             return context;
         }
+
+        public IQueryable<Entity> Get()
+        {
+            return dbSet.AsQueryable();
+        }
+
+        public Entity Get(int id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public virtual void Insert(Entity entity)
+        {
+            dbSet.Add(entity);
+            context.SaveChanges();
+        }
+
+        public virtual void Update(Entity entity, int id)
+        {
+            Entity temp = Get(id);
+            if(temp != null)
+            {
+                context.Entry(temp).CurrentValues.SetValues(entity);
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            Entity temp = Get(id);
+            if (temp != null)
+            {
+                dbSet.Remove(temp);
+                context.SaveChanges();
+            }
+        }
+
     }
 }
