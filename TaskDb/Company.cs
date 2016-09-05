00772ea0e0889215afb78b45802c29cef9608c70
Irma Karasoftić <irmaka@mistral.ba>
2016-09-05 13:23:35 +0200

    Initial commit

---
+++ TaskDb/Company.cs
@@ -0,0 +1,12 @@
+﻿namespace TaskDb
+{
+    public class Company
+    {
+        public int companyId { get; set; }
+        public string name { get; set; }
+        public string streetAddress { get; set; }
+        public string city { get; set; }
+        public int zipCode { get; set; }
+        public string phoneNumber { get; set; }
+    }
+}
---
+++ TaskDb/Item.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskDb
+{
+    public class Item
+    {
+        public int itemId { get; set; }
+        public string description { get; set; }
+        public int quantity { get; set; }
+        public double unitPrice { get; set; }
+    }
+}
