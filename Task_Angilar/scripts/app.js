5028edda03b59203aa11715d94a26283f925662d
Ferhat Avdic <Ferhat Avdic>
2016-09-07 14:09:22 +0200

    angular project added

---
+++ Task_Angilar/scripts/app.js
@@ -0,0 +1,1 @@
+﻿angular.module('taskAngular', ['ngRoute', 'appRoutes', 'MainCtrl']);
---
+++ Task_Angilar/scripts/controllers/MainController.js
@@ -0,0 +1,5 @@
+﻿angular.module('MainCtrl', []).controller('MainController', function ($scope) {
+
+    $scope.tagline = 'To the moon and back!';
+
+});
