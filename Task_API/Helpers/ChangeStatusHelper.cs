f562642ede12fc07c30d7457d991f7cfb89358d8
Irma Karasoftić <irmaka@mistral.ba>
2016-09-09 11:28:17 +0200

    Added yearlyIncomeHelper

--- Task_API/Helpers/ChangeStatusHelper.cs
+++ Task_API/Helpers/ChangeStatusHelper.cs
@@ -13,24 +13,10 @@ namespace Task_API.Helpers
         public static void ChangeStatus(InvoiceModel model, string status, AppContext context)
         {
             Repository<Invoice> invoiceRepository = new Repository<Invoice>(context);
+            EntityParser parser = new EntityParser();
             Invoice invoice = new Invoice();
-            invoice.Id = model.Id;
-            invoice.Date = model.Date;
-            invoice.Customer = context.Customers.Find(model.Customer);
-            invoice.Status = (Status)Enum.Parse(typeof(Status), status);
-
-            foreach (var i in model.Items)
-            {
-                Item item = new Item()
-                {
-                    Id = i.Id,
-                    Description = i.Description,
-                    Quantity = i.Quantity,
-                    UnitPrice = i.UnitPrice
-                };
-                invoice.Items.Add(item);
-            }
-
+            invoice = parser.Create(model, context);
+            
             invoiceRepository.Update(invoice, model.Id);
         }
     }
