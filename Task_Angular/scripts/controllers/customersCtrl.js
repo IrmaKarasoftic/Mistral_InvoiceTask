c028a432ef13857fecff029ad546d5d5e94a3327
Irma Karasoftić <irmaka@WS-4K7VCW1>
2016-09-16 15:15:54 +0200

    added customer view

--- Task_Angular/scripts/controllers/customersCtrl.js
+++ Task_Angular/scripts/controllers/customersCtrl.js
@@ -1,9 +1,25 @@
 ﻿(function () {
     var taskAngular = angular.module('taskAngular');
 
-    taskAngular.controller('customersController', function ($scope) {
+    taskAngular.controller('customersController', function ($scope, dataService) {
 
-      
+        $scope.loadCustomersInfo = function () {
+            dataService.list("customers", function (data) {
+                if (data) {
+                    //console.log(data);
+                    $scope.customers = data;
+                }
+                else {
+                    alert("error");
+                }
+            })
+        };
 
+        $scope.customerTransfer = function (customer) {
+            $scope.requestedCustomer = customer;
+            //console.log($scope.requestedInvoice);
+        };
+
+        $scope.loadCustomersInfo();
     });
 }());
