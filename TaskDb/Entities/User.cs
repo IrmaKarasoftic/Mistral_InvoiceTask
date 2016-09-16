ef581a50867d70a6f935d1d4abcbe6d154a6a161
Irma Karasoftić <irmaka@WS-4K7VCW1>
2016-09-16 14:16:38 +0200

    dzaba commit-am, ne radi login

--- TaskDb/Entities/User.cs
+++ TaskDb/Entities/User.cs
@@ -1,17 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
-using System.Web;
-using TaskDb;
-using TaskDb.Entities;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Task_API.Models
+namespace TaskDb.Entities
 {
-    public class UserModel
+    public class User
     {
         public string Username { get; set; }
         public string Password { get; set; }
     }
-
 }
--- Task_API/Controllers/LoginController.cs
+++ Task_API/Controllers/LoginController.cs
@@ -5,19 +5,38 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using Task_API.Models;
+using TaskDb;
+using TaskDb.Entities;
 
 namespace Task_API.Controllers
 {
     public class LoginController : ApiController
     {
-        public IHttpActionResult Login(UserModel model, string returnUrl)
+        public IHttpActionResult Get(UserModel model, string returnUrl)
         {
-            if(model.Username != null && model.Password != null)
+            if (model.Username != null && model.Password != null)
             {
                 bool isOK = Helpers.LoginHelper.checkLoginInfo(model.Username, model.Password);
                 if (isOK) return Ok();
             }
             return BadRequest();
+        }
+
+        public IHttpActionResult Post(UserModel user)
+        {
+            try
+            {
+                using (AppContext context = new AppContext())
+                {
+                    Repository<User> users = new Repository<User>(context);
+                    Repository<AuthenticationToken> tokens = new Repository<AuthenticationToken>(context);
+                    return Ok();
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
