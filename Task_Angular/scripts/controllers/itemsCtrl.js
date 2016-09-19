feeb51a970fd9926d8d336340b8bb1e3b29dbc66
Ferhat Avdic <Ferhat Avdic>
2016-09-19 15:37:27 +0200

    update item functionality

--- Task_Angular/scripts/controllers/itemsCtrl.js
+++ Task_Angular/scripts/controllers/itemsCtrl.js
@@ -1,11 +1,13 @@
 ﻿(function () {
     var taskAngular = angular.module('taskAngular');
 
-    taskAngular.controller('ItemsController', function ($scope, dataService) {
+    taskAngular.controller('itemsController', function ($scope, dataService) {
 
         $scope.newItemRow = false;
+        $scope.editOnId;
 
         $scope.newItem = {
+            id: 0,
             description: "",
             quantity: 0,
             unitPrice: ""
@@ -23,25 +25,67 @@
         };
 
         $scope.createNewItem = function () {
-            if($scope.newItem)
             dataService.create("items", $scope.newItem, function (data) {
                 if (data)
                 {
                     $scope.hideNewItemRow();
-                    alert("item created");
+                    alert("Item created");
                 }
                 else
+                    alert("Error");
+            })
+        }
+
+        $scope.updateItem = function () {
+            dataService.update("items", $scope.newItem.id, $scope.newItem, function (data) {
+                $scope.editOff();
+                if (data) {
+                    alert("Item updated");
+                }
+                else {
                     alert("error");
+                }
+            })
+        }
+
+        $scope.removeItem = function () {
+            dataService.remove("items", $scope.newItem.id, function (data) {
+                $scope.editOff();
+                if (data) {
+                    alert("Item removed");
+                }
+                else {
+                    alert("error");
+                }
             })
         }
 
         $scope.showNewItemRow = function (){
             $scope.newItemRow = true;
+            $scope.editOff();
         }
 
         $scope.hideNewItemRow = function () {
             $scope.newItemRow = false;
+            $scope.newItem = null;
         }
+
+        $scope.editOn = function (item) {
+            $scope.hideNewItemRow();
+            $scope.editOnId = item.id;
+            $scope.newItem = item;
+        }
+
+
+        $scope.editOff = function () {
+            $scope.editOnId = null;
+            $scope.newItem = null;
+        }
+
+        $scope.checkEdit = function (item) {
+            return $scope.editOnId === item.id;
+        }
+
 
         $scope.loadItemsInfo();
 
