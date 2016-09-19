1eb0ebfa5638d4a5048289d62c597643f41cd935
Irma Karasoftić <irmaka@WS-4K7VCW1>
2016-09-19 15:16:46 +0200

    added some more front-end code

---
+++ Task_Angular/scripts/controllers/newCustomerCtrl.js
@@ -0,0 +1,52 @@
+﻿(function () {
+    var taskAngular = angular.module('taskAngular');
+
+    taskAngular.controller('newCustomerController', function ($scope, dataService) {
+
+        $scope.newCustomerRow = false;
+
+        $scope.newCustomer = {
+            name: "",
+            company: 0,
+            companyName: "",
+            streetAddress: "",
+            city: "",
+            zipCode: 0,
+            phoneNumber:""
+        }
+
+        $scope.loadCustomersInfo = function () {
+            dataService.list("customers", function (data) {
+                if (data) {
+                    $scope.customers = data;
+                }
+                else {
+                    alert("error");
+                }
+            })
+        };
+
+        $scope.createNewCustomer = function () {
+            if ($scope.newCustomer)
+                dataService.create("customers", $scope.newCustomer, function (data) {
+                    if (data) {
+                        $scope.hideNewCustomerRow();
+                        alert("customer created");
+                    }
+                    else
+                        alert("error");
+                })
+        }
+
+        $scope.showNewCustomerRow = function () {
+            $scope.newCustomerRow = true;
+        }
+
+        $scope.hideNewCustomerRow = function () {
+            $scope.newCustomerRow = false;
+        }
+
+        $scope.loadCustomersInfo();
+
+    });
+}());
