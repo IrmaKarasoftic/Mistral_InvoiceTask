87506e117a2b53d20e0e33a13e3a303110ec9cfc
Irma Karasoftić <irmaka@WS-4K7VCW1>
2016-09-20 11:52:50 +0200

    worked on customers view

--- Task_Angular/scripts/controllers/customersCtrl.js
+++ Task_Angular/scripts/controllers/customersCtrl.js
@@ -17,9 +17,58 @@
 
         $scope.customerTransfer = function (customer) {
             $scope.requestedCustomer = customer;
-            //console.log($scope.requestedInvoice);
+            $scope.editCustomer = $.extend(true, {}, customer);
         };
 
+        $scope.newCustomer = {
+            id: 0,
+            name: "",
+            company: 0,
+            companyName: "",
+            streetAddress: "",
+            city: "",
+            zipCode: 0,
+            phoneNumber: ""
+        }
+
+
+
+        $scope.createNewCustomer = function () {
+            //if ($scope.newCustomer)
+            dataService.create("customers", $scope.newCustomer, function (data) {
+                if (data) {
+                    alert("Customer created");
+                }
+                else
+                    alert("error");
+            })
+        }
+
+
+        $scope.updateCustomer = function () {
+            dataService.update("customers", $scope.editCustomer.id, $scope.editCustomer, function (data) {
+                $scope.loadCustomersInfo();
+                if (data) {
+                    alert("Customer updated");
+                }
+                else {
+                    alert("error");
+                }
+            })
+        }
+
+        $scope.removeCustomer = function () {
+            dataService.remove("customers", $scope.newCustomer.id, function (data) {
+                if (data) {
+                    alert("Customer removed");
+                }
+                else {
+                    alert("error");
+                }
+            })
+        }
+
         $scope.loadCustomersInfo();
+
     });
 }());
